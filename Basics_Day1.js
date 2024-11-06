a90f8a7b084c09c7855ced2a920a6e2e5e0dd812
Pankaj Nidhi <46417970+pj-nidhi@users.noreply.github.com>
2024-11-06 06:48:02 +0000

    Basic Html and JS

---
+++ Basics_Day1.js
@@ -0,0 +1,15 @@
+
+const custId = 1231;
+let Email = String;
+let Password = String;
+
+// Email = document.getElementsByClassName('Email')
+// Password = document.getElementsByClassName('Password')
+
+//Email = InputEvent
+
+console.log(Email);
+console.log(Password);
+
+
+console.log('Hello JS');
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 created this respo to track my progress on java script. 
 Journy
 //Day 1 - setting up environment
+//Day 2 - creating Form
