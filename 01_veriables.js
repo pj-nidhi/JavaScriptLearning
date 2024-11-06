5ce6714c95e5c77b4c6fb844c6cc2dd8daaf7fd1
Pankaj Nidhi <46417970+pj-nidhi@users.noreply.github.com>
2024-11-06 06:59:45 +0000

    Datatypes and veriables scope

---
+++ 01_veriables.js
@@ -0,0 +1,26 @@
+const userId = 1231;
+var email = 'Pankaj@gmail.com';
+let pass = '123123'
+
+var email = 'nidhi@gmail.com';
+pass = 1232;
+
+console.log(email);
+console.log(pass);
+ 
+{
+    let pass = 10;
+    var email ='abc@gmail.com';
+    console.log(pass);
+}
+console.log(pass);
+console.log(email);
+
+//Data Types
+String
+Boolean
+Number
+undefined
+null
+
+
