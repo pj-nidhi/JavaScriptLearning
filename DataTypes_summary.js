6bfebb86e52bc89eef95db8e914fc4a032430330
Pankaj Nidhi <46417970+pj-nidhi@users.noreply.github.com>
2024-11-08 07:52:35 +0000

    datatype summary

---
+++ DataTypes_summary.js
@@ -0,0 +1,27 @@
+//Premative
+
+// datatypes : String Number Boolean bigInt 
+
+//NON Premative 
+
+// Array Objects Functions
+
+
+const heros = ["shaktiman", "superman","batman"];
+
+console.log(heros);
+
+let myObj={
+    name:"Shakti",
+    age: 69
+}
+
+console.log(myObj.name);
+
+
+const myFunction = function (){
+console.log("hello world");
+
+}
+
+myFunction()
