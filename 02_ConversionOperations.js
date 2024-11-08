be0c730595a3bec0485e4c1bff579c2378193320
Pankaj Nidhi <46417970+pj-nidhi@users.noreply.github.com>
2024-11-08 07:29:26 +0000

    Day3

---
+++ 02_ConversionOperations.js
@@ -0,0 +1,23 @@
+
+
+let score = String(20);
+
+let numScore = Number(score)
+console.log(typeof score);
+console.log(numScore , typeof numScore);
+
+let varBool;
+let boolScore = Boolean(varBool);
+console.log(boolScore , typeof boolScore);
+
+console.log('4'+(1+2)+'3');
+console.log(true+false);
+
+let count = 1
+count++;
+console.log(count++);
+console.log(++count);
+
+
+
+
