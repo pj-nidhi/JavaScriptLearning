172282f274c282c87139db85903f17d9927616a0
Pankaj Nidhi <46417970+pj-nidhi@users.noreply.github.com>
2024-11-18 07:40:17 +0000

    CLICK event

--- Basics_Day1.js
+++ Basics_Day1.js
@@ -1,15 +1,17 @@
 
 const custId = 1231;
-let Email = String;
-let Password = String;
-
-Email = document.getElementById('Email').value
-Password = document.getElementById('Password').value
 
 //Email = InputEvent
 
-console.log(Email);
+console.log('Hello JS');
+
+function getData(){
+    let Email = String;
+    let Password = String;
+
+    Email = document.getElementById('Email').value
+    Password = document.getElementById('Password').value
+    console.log(Email);
 console.log(Password);
 
-
-console.log('Hello JS');
+}
